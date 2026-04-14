af92f44be9b2c455282841370442cede47ef2f88
Amirul <amirulislam9.e@gmail.com>
2026-04-14 16:33:26 +0600

    navigation add in header

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import { House, Clock2, ChartLine } from "lucide-react";
+import Link from "next/link";
+
+function Navbar() {
+  return (
+    <header className="w-full shadow-lg py-2.5 flex items-center justify-between lg:px-20 mx-auto">
+      <h1 className=" font-black text-2xl">
+        Keen<span className=" font-semibold text-[#244D3F] ">Keeper</span>
+      </h1>
+
+      <nav className="flex  items-center justify-between gap-4 text-base">
+        <Link href={"/"}>
+          <House size={14} className="mr-1 inline" />
+          Home
+        </Link>
+        <Link href={"/timeline"}>
+          <Clock2 size={14} className="mr-1 inline" />
+          Timeline
+        </Link>
+        <Link href={"/stats"}>
+          <ChartLine size={14} className="mr-1 inline" />
+          Stats
+        </Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
