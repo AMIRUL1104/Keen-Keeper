93043f065da2d60e727e089254d5239e3da6cf2a
Amirul <amirulislam9.e@gmail.com>
2026-04-14 22:21:34 +0600

    stats section and friends section add in homepage

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,10 +21,9 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      data-theme="light"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col mx-auto  bg-white max-w-400">
+      <body className="min-h-full flex flex-col mx-auto  font-sans  bg-zinc-50 max-w-400">
         <Navbar />
         {children}
       </body>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,13 @@
 import Banner from "./Banner/Banner";
+import Friends from "./Friends/Friends";
+import Stats from "./Stats/Stats";
 
 function Home() {
   return (
     <div>
       <Banner />
+      <Stats />
+      <Friends />
     </div>
   );
 }
