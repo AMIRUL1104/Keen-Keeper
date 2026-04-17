8a61472f844f76a8340f44197126418dd0a2c04c
Amirul <amirulislam9.e@gmail.com>
2026-04-17 22:58:03 +0600

    remove unused code and unnecessery comments

--- src/app/stats/page.jsx
+++ src/app/stats/page.jsx
@@ -5,9 +5,6 @@ import { useContext, useEffect } from "react";
 import { Pie, PieChart, Cell, ResponsiveContainer } from "recharts";
 
 export default function StatsPage({ isAnimationActive = true }) {
-  // const storedData = localStorage.getItem("timelineData");
-  // const data = storedData ? JSON.parse(storedData) : [];
-
   const { data, setData } = useContext(TimelineContext);
   useEffect(() => {
     const storedData = localStorage.getItem("timelineData");
@@ -81,89 +78,3 @@ export default function StatsPage({ isAnimationActive = true }) {
     </div>
   );
 }
-
-// "use client";
-
-// import { useState, useEffect } from "react"; // useState এবং useEffect ইমপোর্ট করুন
-// import { Pie, PieChart, Cell, ResponsiveContainer } from "recharts";
-
-// export default function StatsPage({ isAnimationActive = true }) {
-//   // ১. ডেটা রাখার জন্য একটি স্টেট তৈরি করুন
-//   const [data, setData] = useState([]);
-//   // const [mounted, setMounted] = useState(false);
-
-//   // ২. useEffect ব্যবহার করে ব্রাউজারে ডেটা লোড করুন
-//   useEffect(() => {
-//     const storedData = localStorage.getItem("timelineData");
-//     if (storedData) {
-//       setData(JSON.parse(storedData));
-//     }
-//     // setMounted(true); // মাউন্ট হয়েছে কিনা তা নিশ্চিত করতে
-//   }, []);
-
-//   // Hydration mismatch এড়াতে এটি প্রয়োজন
-//   // if (!mounted) return null;
-
-//   const chartData = [
-//     { name: "Call", value: 0, fill: "#244D3F" },
-//     { name: "Text", value: 0, fill: "#00C49F" },
-//     { name: "Video", value: 0, fill: "#8528ff" },
-//   ];
-
-//   if (data.length !== 0) {
-//     data.forEach((element) => {
-//       if (element.label.toLowerCase() === "call") {
-//         chartData.value += 1;
-//       } else if (element.label.toLowerCase() === "text") {
-//         chartData.value += 1;
-//       } else {
-//         chartData.value += 1;
-//       }
-//     });
-//   }
-
-//   if (data.length === 0) {
-//     return (
-//       <div className="flex items-center flex-col justify-center h-screen w-full">
-//         <h1 className="sm:text-4xl text-2xl font-bold text-gray-800 text-left mb-4">
-//           Friendship Analytics
-//         </h1>
-//         <p className="text-gray-500 text-sm">
-//           No data available. Please check in with your friends to see stats
-//           here.
-//         </p>
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <div className="flex items-center flex-col justify-center h-screen w-full">
-//       <h1 className="sm:text-4xl text-2xl font-bold text-gray-800 text-left mb-4">
-//         Friendship Analytics
-//       </h1>
-//       <div className="w-full max-w-137.5 aspect-square max-h-[80vh] bg-white rounded-xl shadow-md p-6">
-//         <p className="text-lg font-semibold text-left mb-4">
-//           By interaction type
-//         </p>
-//         <ResponsiveContainer width="100%" height="100%">
-//           <PieChart>
-//             <Pie
-//               data={chartData}
-//               innerRadius="40%"
-//               outerRadius="55%"
-//               cornerRadius={6}
-//               paddingAngle={5}
-//               dataKey="value"
-//               isAnimationActive={isAnimationActive}
-//               label={({ name, value }) => `${name}: ${value}`}
-//             >
-//               {chartData.map((entry, index) => (
-//                 <Cell key={`cell-${index}`} fill={entry.fill} />
-//               ))}
-//             </Pie>
-//           </PieChart>
-//         </ResponsiveContainer>
-//       </div>
-//     </div>
-//   );
-// }
