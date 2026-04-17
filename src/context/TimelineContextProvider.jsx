18a8b1461a482d5bf99e8cee44b7ade840a24a51
Amirul <amirulislam9.e@gmail.com>
2026-04-17 22:28:24 +0600

    readme.md file updated

--- src/context/TimelineContextProvider.jsx
+++ src/context/TimelineContextProvider.jsx
@@ -2,20 +2,6 @@
 
 import { TimelineContext } from "./TimelineContest";
 
-// function TimelineContextProvider({ children }) {
-// // get data from localStorage
-//   const storedData = localStorage.getItem("timelineData");
-//   const initialData = storedData ? JSON.parse(storedData) : [];
-//   const [data, setData] = useState(initialData);
-
-//   const datas = { data, setData };
-//   return (
-//     <TimelineContext.Provider value={datas}>
-//       {children}
-//     </TimelineContext.Provider>
-//   );
-// }
-
 import { useState, useEffect } from "react";
 
 function TimelineContextProvider({ children }) {
@@ -26,7 +12,7 @@ function TimelineContextProvider({ children }) {
     if (storedData) {
       setData(JSON.parse(storedData));
     }
-  }, []); // এটি শুধুমাত্র কম্পোনেন্ট মাউন্ট হওয়ার পর একবার চলবে
+  }, []);
 
   const datas = { data, setData };
 
